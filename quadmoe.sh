ba59da4f84b00e8cfbba20264326e337036b548c
Samadi van Koten <samadi@vktec.co.uk>
2017-12-13 19:29:44 +0000

    Add argument validation to quadmoe.sh

--- quadmoe.sh
+++ quadmoe.sh
@@ -2,5 +2,10 @@
 # Upload files to a QuadFile instance
 # Requires jq
 
+if [ "$#" -eq 0 ]; then
+  echo "Usage: quadmoe.sh <file>" >&2
+  exit 1
+fi
+
 url="https://file.quad.moe/" # Change this to use a different server
 curl -F "file=@$1" "$url" | jq -r .url
